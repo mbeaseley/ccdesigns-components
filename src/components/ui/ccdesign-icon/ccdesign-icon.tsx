a836adf33cb51837e4cb70f398b2b95653de3c2f
Michael Beaseley <m.t.beaseley@gmail.com>
2018-11-07 01:52:04 +0000

    dev: update icon component to see icon svg plus colour it (Still does not work yet)

--- src/components/ui/ccdesign-icon/ccdesign-icon.tsx
+++ src/components/ui/ccdesign-icon/ccdesign-icon.tsx
@@ -6,6 +6,8 @@ import { Component, Prop, Element } from '@stencil/core';
 })
 export class CcdesignIcon {
   @Prop() iconName: string;
+  @Prop() iconSize: string = "icon--xs";
+  @Prop() iconColor: string = "icon--white";
 
   @Element() iconEl: HTMLElement;
 
@@ -14,24 +16,29 @@ export class CcdesignIcon {
     let ajax = new XMLHttpRequest();
 
     ajax.open("GET",`https://ccdesigns.blob.core.windows.net/icons/${this.iconName}` + ".svg", true);
-    // ajax.onload = () => {
-    //   // let iconResponse = ajax.responseText;
-    //   // iconResponse = iconResponse.replace(
-    //   //   `<svg`,
-    //   //   `<svg id=${this.iconName}`
-    //   // );
-    //   iconResult.innerHTML = ajax.responseText;
-    // };
-    // ajax.send();
-
-    console.log(iconResult);
-    debugger;
+    ajax.onload = () => {
+      let iconResponse = ajax.responseText;
+      iconResponse = iconResponse.replace(
+        `layer_1`,
+        `${this.iconName}`
+      );
+      iconResult.innerHTML = iconResponse;
+    };
+    ajax.send();
   }
 
   render() {
+    let inputColor;
+    const backgroundColors = ["white", "light-grey", "grey", "dark-grey", "black"];
+    if (backgroundColors.indexOf(this.iconColor) > -1) {
+      inputColor = `icon--${this.iconColor}`;
+    } else {
+      console.error(`${this.iconColor} is not a defined color. Default color is used.`);
+      inputColor = `icon--dark-grey`;
+    }
     return (
-      <svg>
-        <use/>
+      <svg class={`icon ${inputColor}`}>
+        <use xhreflink={`#${this.iconName}`}/>
       </svg>
     );
   }
