123cfb5111cf8e22f5c5b01fe9df2aeb20a29f75
Michael Beaseley <m.t.beaseley@gmail.com>
2018-11-13 09:26:57 +0000

    dev: update template component to take header component

--- src/components/template/ccdesign-cv-template/ccdesign-cv-template.tsx
+++ src/components/template/ccdesign-cv-template/ccdesign-cv-template.tsx
@@ -5,12 +5,15 @@ import { Component, Prop } from '@stencil/core';
   styleUrl: 'ccdesign-cv-template.scss'
 })
 export class CcdesignCvTemplate {
+  @Prop() headerEntry: string;
   @Prop() footerEntry: string;
 
   render() {
     return (
       <div>
-        <ccdesign-header></ccdesign-header>
+        <ccdesign-header
+          entry-id={`${this.headerEntry}`}>
+        </ccdesign-header>
 
         <main class="page">
           <content class="page__body">
