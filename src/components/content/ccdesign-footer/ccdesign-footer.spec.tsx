d29adddbcbe8da04436f77b7ef23e44429987da1
Michael Beaseley <m.t.beaseley@gmail.com>
2019-01-01 22:51:41 +0000

    dev: create footer spec

--- src/components/content/ccdesign-footer/ccdesign-footer.spec.tsx
+++ src/components/content/ccdesign-footer/ccdesign-footer.spec.tsx
@@ -1,7 +1,23 @@
 import { CcdesignFooter } from './ccdesign-footer';
+import { h } from '@stencil/core/testing';
+
+const mockFooterData = {
+  id: 'example', 
+  text: 'example text',
+  backgroundColor: 'exampleColor'
+};
 
 describe('ccdesign-footer', () => {
   it('should build', () => {
     expect(new CcdesignFooter()).toBeTruthy();
   });
+
+  describe('footer functionality', () => {
+    it('should process footer object', () => {
+      const result = h('<ccdesign-footer></ccdesign-footer>', mockFooterData);
+      expect(result.vattrs.id).toBe(mockFooterData.id);
+      expect(result.vattrs.text).toBe(mockFooterData.text);
+      expect(result.vattrs.backgroundColor).toBe(mockFooterData.backgroundColor);
+    });
+  })
 });
