68a082c562c15c0c9a074ed8e1f9d1ab119f8e2f
Michael Beaseley <m.t.beaseley@gmail.com>
2019-01-02 13:40:42 +0000

    dev: update social container component spec

--- src/components/content/ccdesign-social-container/ccdesign-social-container.spec.tsx
+++ src/components/content/ccdesign-social-container/ccdesign-social-container.spec.tsx
@@ -1,28 +1,7 @@
-import { TestWindow } from '@stencil/core/testing';
 import { CcdesignSocialContainer } from './ccdesign-social-container';
 
 describe('ccdesign-social-container', () => {
   it('should build', () => {
     expect(new CcdesignSocialContainer()).toBeTruthy();
-  });
-
-  describe('rendering', () => {
-    let element;
-    let window;
-
-    beforeEach(async () => {
-      window = new TestWindow();
-      element = await window.load({
-        components: [CcdesignSocialContainer],
-        html: '<ccdesign-social-container></ccdesign-social-container>'
-      });
-    });
-
-    it('should work with both the first and the last name', async () => {
-      element.first = 'Peter';
-      element.last = 'Parker';
-      await window.flush();
-      expect(element.textContent).toEqual('Hello, my name is Peter Parker');
-    });
-  });
+  }); 
 });
