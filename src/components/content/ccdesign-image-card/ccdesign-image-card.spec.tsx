81f7c85789ce0074de0456c2495529be51818abd
Michael Beaseley <m.t.beaseley@gmail.com>
2019-01-28 15:04:17 +0000

    dev: create image card component

---
+++ src/components/content/ccdesign-image-card/ccdesign-image-card.spec.tsx
@@ -0,0 +1,7 @@
+import { CcdesignImageCard } from './ccdesign-image-card';
+
+describe('ccdesign-image-card', () => {
+  it('should build', () => {
+    expect(new CcdesignImageCard()).toBeTruthy();
+  });
+});
