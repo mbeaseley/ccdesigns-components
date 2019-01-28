5d3d2351e6ca45f67eff5a2b144376e28e879beb
Michael Beaseley <m.t.beaseley@gmail.com>
2019-01-28 15:03:17 +0000

    dev: create grid component

---
+++ src/components/content/ccdesign-grid/ccdesign-grid.spec.tsx
@@ -0,0 +1,7 @@
+import { CcdesignGrid } from './ccdesign-grid';
+
+describe('ccdesign-grid', () => {
+  it('should build', () => {
+    expect(new CcdesignGrid()).toBeTruthy();
+  });
+});
---
+++ src/components/content/ccdesign-grid/ccdesign-grid.tsx
@@ -0,0 +1,26 @@
+import { Component, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'ccdesign-grid',
+  styleUrl: 'ccdesign-grid.scss'
+})
+export class CcdesignGrid {
+  @Prop() heading: string;
+
+  render() {
+    const header = (
+      <div class="grid__header">
+        <h1>{ this.heading }</h1>
+      </div>
+    );
+
+    return (
+      <div class="grid">
+        { this.heading ? header : '' }
+        <div class="grid__container">
+          <slot name="children" />
+        </div>
+      </div>
+    );
+  }
+}
